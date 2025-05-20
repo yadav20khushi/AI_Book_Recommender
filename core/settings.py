733fc43b66a04785f13c06ede8f577ce4a6c8e00
Khushi Yadav <yadav20khushi@gmail.com>
2025-05-20 15:53:24 +0530

    completed external_api/services.py

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import sys
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # load .env variables
+
+EXTERNAL_API_KEY = os.getenv("EXTERNAL_API_KEY")
+EXTERNAL_API_BASE_URL = os.getenv("EXTERNAL_API_BASE_URL")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
