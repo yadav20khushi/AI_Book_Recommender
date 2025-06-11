6a64eac7917087f7ec0d8b0331a7143a6ea5ec8c
Khushi Yadav <yadav20khushi@gmail.com>
2025-06-11 12:55:49 +0530

    home page.html

--- static/js/sidebar.js
+++
@@ -1,6 +0,0 @@
-function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const main = document.querySelector('.main-content');
-    sidebar.classList.toggle('collapsed');
-    main.classList.toggle('collapsed');
-}
