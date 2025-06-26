63e550b4657699e87e4d40b052f875c9d9e00b68
Akshara Sharma <139435323+AksharaaSharma@users.noreply.github.com>
2025-06-26 11:44:23 +0530

    search bar and video generation functionality

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+#python library_genta_django\manage.py runserver
+#http://127.0.0.1:8000/chat/
+
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_genta_django.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
